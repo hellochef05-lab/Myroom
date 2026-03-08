937ba61bb90410493beeb536ff453c4c4aa18553
Akshay Kumar <akshay@MacBookPro.localdomain>
2026-03-08 10:25:41 +0400

    remove workflow file from push

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
@@ -6,16 +5,3 @@ export default defineConfig({
   plugins: [react()],
   base: '/Myroom/',
 })
-=======
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      "/api": "http://localhost:3001",
-    },
-  },
-});
->>>>>>> 98980c65769d3697767a8c663ba29d1185cd36ce
